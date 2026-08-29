1ce88b9ebe8e4a2343fce8b441480b1ece3d4c4a
mehedi-01 <mehedi.dev01@gmail.com>
2026-08-29 21:39:31 +0600

    new added code

---
+++ bin/loop_practice.dart
@@ -0,0 +1,13 @@
+// 1 to 10 print
+void main(){
+// for (int i = 1; i<= 10; i++){
+//    print(i);
+//    }
+//   for(int i= 10; i>= 1; i--){
+//     print(i);
+//   }
+     for (int i = 1; i<=20; i++){
+       print(i);
+     }
+ }
+
